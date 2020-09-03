aa58a5707efa61c4fb0632d26c5098614931a757
Juan Villacorta <juanvillacortac@gmail.com>
2020-09-03 18:05:37 -0400

    Index

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -2,10 +2,10 @@
   const hour = new Date().getHours()
   let greetings: string
 
-  if (hour < 12) { greetings = 'Buenos días' }
-  if (hour == 12) { greetings = 'Buen provecho' }
-  if (hour > 12) { greetings = 'Buenas tardes' }
-  if (hour > 18) { greetings = 'Buenas noches' }
+  if (hour < 12) { greetings = '🌄 Buenos días' }
+  if (hour == 12) { greetings = '🍛 Buen provecho' }
+  if (hour > 12) { greetings = '🌇 Buenas tardes' }
+  if (hour > 18) { greetings = '🌃 Buenas noches' }
 </script>
 
 <h1 class="text-4xl font-satisfy">{greetings}, morrocoyita hermosa</h1>
